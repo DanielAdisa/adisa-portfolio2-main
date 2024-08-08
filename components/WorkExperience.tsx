8762a2d92a85a3ecfa53a766348f551b54a4c231
Your Name <you@example.com>
2024-08-08 08:30:52 +0100

    mt-2

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -19,7 +19,7 @@ export default function WorkExperience({experiences}: Props) {
         <h3  className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-xl md:text-2xl'>
             Experience
         </h3>
-        <div className=' w-5/4 flex content-center  overflow-x-scroll p-4 space-x-3 snap-x snap-center snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#25D366]/80 mt-8'>
+        <div className=' w-5/4 flex content-center  overflow-x-scroll p-4 space-x-3 snap-x snap-center snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#25D366]/80 mt-2'>
             {/* Experience Cards */}
             {experiences?.map((experience) => (
               <ExperienceCard key={experience._id} experience = {experience} />

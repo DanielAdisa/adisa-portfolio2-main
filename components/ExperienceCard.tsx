928d347a89572f878a5298921946dba263d33eba
Your Name <you@example.com>
2024-08-08 14:52:08 +0100

    trials#1

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function ExperienceCard({experience}: Props) {
   return (
-    <article className='flex flex-col rounded-l space-y-7 flex-shrink-0 w-[350px] h-[80vh] md:w-[600px] md:h-[820px] xl:w-[800px] snap-center mt-20 bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-y-scroll'>
+    <article className='flex flex-col rounded-lg space-y-7 flex-shrink-0 w-[35vh] h-[80vh] md:w-[600px] md:h-[820px] xl:w-[800px] snap-center mt-20 bg-[#292929] p-8 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-y-scroll scrollbar-thin scrollbar-track-black scrollbar-thumb-[#25D366]/80'>
         <motion.div
         initial={{y:-100, opacity:0,}}
         transition={{duration:1.2}}
@@ -25,9 +25,9 @@ export default function ExperienceCard({experience}: Props) {
             className=' w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover'
             />
         </motion.div>
-        <div className='px-0 md:px-10'>
-            <h4 className='text-4xl font-light'>CEO Misfits</h4>
-            <p className='font-bold text-2xl mt-1'>Misfits</p>
+        <div className='px-0 md:px-5'>
+            <h4 className='md:text-4xl text-2xl font-bold'>{experience?.jobTitle}</h4>
+            <p className=' text-2xl mt-1 font-light'>{experience?.company}</p>
             <div className=' flex space-x-2 my-2'>
                 {experience.technologies.map(technology =>(
                     <Image src={urlFor(technology.image).url()}
@@ -46,7 +46,7 @@ export default function ExperienceCard({experience}: Props) {
                 }
 
             </p>
-            <ol className='px-0 md:px-10 font-mono  md:text-lg list-disc space-y-4 ml-5 max-w-90 pr-10 text-lg max-h-100 overflow-y-hidden scrollbar-thin scrollbar-track-black scrollbar-thumb-[#25D366]/80'>
+            <ol className='px-0 md:px-5 font-mono  md:text-lg list-disc space-y-4 ml-5 max-w-90 pr-10 text-lg max-h-100 overflow-y-hidden scrollbar-thin scrollbar-track-black scrollbar-thumb-[#25D366]/80'>
                 {experience.points.map((point, i) =>(
                     <li key={i}>{point}</li>
                 ))}

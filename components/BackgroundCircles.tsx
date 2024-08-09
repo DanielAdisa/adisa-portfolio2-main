dda7ec4f607369e15767184d49750fdc4e200a24
Your Name <you@example.com>
2024-08-09 18:06:15 +0100

    nocircles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -22,11 +22,11 @@ export default function BackgroundCircles({}: Props) {
       }}
     
     className=' relative flex justify-center items-center'>
-        <div className='absolute border-[#333333] border rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
+        {/* <div className='absolute border-[#333333] border rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
         <div className='absolute border-[#333333] border rounded-full h-[300px] w-[300px] mt-52 animate-pulse'/>
         <div className='absolute border-[#333333] border rounded-full h-[500px] w-[500px] mt-52 animate-pulse'/>
         <div className='absolute border-[#25D366] border  rounded-full opacity-20 h-[650px] w-[650px] mt-52 animate-ping'/>
-        <div className='absolute border-[#25D366] border  rounded-full opacity-20 h-[700px] w-[700px] mt-52 animate-pulse'/>
+        <div className='absolute border-[#25D366] border  rounded-full opacity-20 h-[700px] w-[700px] mt-52 animate-pulse'/> */}
     </motion.div>
   )
 }
